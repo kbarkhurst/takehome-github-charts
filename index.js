319590f4672a7b0bc633e4d238dc796d1203f7c2
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-06-07 23:31:25 -0400

    Stil trying to get Axios

--- index.js
+++ index.js
@@ -1,48 +1,65 @@
 /* global Chart, axios */
-
-let vueData = () => {
+// let vuewatcher = [];
+const lst = [];  
+const populateData = (data) => {
+  lst.push(data); 
+};
+function axiosTest (populateData) {
   axios.get("https://api.github.com/repos/vuejs/vue")
+    .then(function(response) {
+      populateData(response.data.watchers);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+axiosTest();
+console.log("Lst" + lst);
+console.log("Data" + populateData);
+
+
+function vueData() {
+  axios
+    .get("https://api.github.com/repos/vuejs/vue")
     .then(function (response) {
       console.log(response.data);
-      let vuewatcher = response.data.watchers;
-      document.getElementById("vuewatcher").innerHTML = vuewatcher;
-      console.log("Vue.js Watchers:" + response.data.watchers);
-      
-
-
-      const data = {
-        labels: [
-          'Vue',
-          'Angular',
-          'Ember'
-        ],
-        datasets: [{
-          label: 'Watchers',
-          data: [vuewatcher, 1000, 5000],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-          hoverOffset: 4
-        }]
-      };
- 
-      const config = {
-        type: 'pie',
-        data: data,
-      };
-      // === include 'setup' then 'config' above ===
-      var myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-      );
+      // let vuewatcher = response.data.watchers;
+      // document.getElementById("vuewatcher").innerHTML = vuewatcher;
+      // console.log("Vue.js Watchers:" + response.data.watchers);
+      // this.vuewatcher = response.data.watchers;
+      return response.data;
     });
-  // .catch(function (error) {
-  //   console.log(error);
-};
+}
+console.log("VueData:" + vueData());
 
-    
+const data = {
+  labels: [
+    'Vue',
+    'Angular',
+    'Ember'
+  ],
+  datasets: [{
+    label: 'Watchers',
+    data: [vueData(), 1000, 5000],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
+ 
+const config = {
+  type: 'pie',
+  data: data,
+};
+// === include 'setup' then 'config' above ===
+var myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
+      
 
 
 
