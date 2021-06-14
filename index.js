067102a5a8c7978c38b7c310254ce672afd81938
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-06-14 18:41:02 -0400

    Tried promise within promise, didn't work.

--- index.js
+++ index.js
@@ -43,61 +43,78 @@ function axiosTest() {
 // console.log("Lst" + lst);
 // console.log("Data" + populateData);
 
-let vuewatcher = null;
+
 
 function vueData() {
-  axios
-    .get("https://api.github.com/repos/vuejs/vue")
-    .then(function (response) {
-      console.log(response.data);
-      let vuewatcher = response.data.watchers;
-      document.getElementById("vuewatcher").innerHTML = vuewatcher;
-      console.log("Vue.js Watchers:" + response.data.watchers);
-      this.vuewatcher = response.data.watchers;
-      console.log("Vue.js Watchers2:" + vuewatcher);
-      
-      
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  return axios({
+    url: 'https://api.github.com/repos/vuejs/vue',
+    method: 'get',
+    timeout: 8000,
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+    .then(response => response.data)
+    .catch(error => console.error(error));
 }
-
-function doStuff() {
-  // let's make this a wrapper function
-  // make other functions async
-  // get the data once it's done - await the axios request
-  // make the chart
-}
-function makeChart() {
-  const data = {
-    labels: [
-      'Vue',
-      'Angular',
-      'Ember'
-    ],
-    datasets: [{
-      label: 'Watchers',
-      data: ["vuewatcher", 1000, 5000],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
-  };
- 
-  const config = {
-    type: 'pie',
-    data: data,
-  };
-  // === include 'setup' then 'config' above ===
-  var myChart = new Chart(
-    document.getElementById('myChart'),
-    config
+vueData()
+  .then(
+    response => console.log('vueData' + response.data)
   );
-}
+  
+
+//     .get("https://api.github.com/repos/vuejs/vue")
+//     .then(function (response) {
+//       console.log(response.data);
+//       let vuewatcher = response.data.watchers;
+//       document.getElementById("vuewatcher").innerHTML = vuewatcher;
+      
+//       console.log("Vue.js Watchers:" + response.data.watchers);
+//       return vuewatcher;
+//       // this.vuewatcher = response.data.watchers;
+//       // console.log("Vue.js Watchers2:" + vuewatcher);
+      
+      
+//     });
+// }
+
+// window.doStuff = function() {
+//   vueData();
+//   console.log("From other function:" + vueData());
+//   // let's make this a wrapper function
+//   // make other functions async
+//   // get the data once it's done - await the axios request
+//   // make the chart
+// }
+// function makeChart() {
+//   const data = {
+//     labels: [
+//       'Vue',
+//       'Angular',
+//       'Ember'
+//     ],
+//     datasets: [{
+//       label: 'Watchers',
+//       data: ["vuewatcher", 1000, 5000],
+//       backgroundColor: [
+//         'rgb(255, 99, 132)',
+//         'rgb(54, 162, 235)',
+//         'rgb(255, 205, 86)'
+//       ],
+//       hoverOffset: 4
+//     }]
+//   };
+ 
+//   const config = {
+//     type: 'pie',
+//     data: data,
+//   };
+//   // === include 'setup' then 'config' above ===
+//   var myChart = new Chart(
+//     document.getElementById('myChart'),
+//     config
+//   );
+// }
   
   
-console.log("VueData:" + vueData());
+// console.log("VueData:" + vueData());
