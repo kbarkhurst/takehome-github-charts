ba993e1dd20da07953dcdaab017af6c6d9a86876
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-06-14 22:15:39 -0400

    Almost have watchers done

--- index.js
+++ index.js
@@ -46,93 +46,95 @@
 
 
 async function vueData() {
-  const response = await axios.get("https://api.github.com/repos/vuejs/vue")
-  const data = await response;
-  console.log("vueData Vue Watchers:" + data.data.watchers);
-  let vuewatcher = response.data.watchers;
+  const [vueResponse, angularResponse, emberjsResponse, svelteResponse] = await Promise.all([
+    axios.get("https://api.github.com/repos/vuejs/vue"),
+    axios.get("https://api.github.com/repos/angular/angular"),
+    axios.get("https://api.github.com/repos/emberjs/ember"),
+    axios.get("https://api.github.com/repos/sveltejs/svelte")
+  ]);
+  const data = await [vueResponse, angularResponse, emberjsResponse, svelteResponse];
+  
+
+  let vuewatcher = vueResponse.data.watchers;
+  let angularwatcher = angularResponse.data.watchers;
+  let emberjswatcher = emberjsResponse.data.watchers;
+  let sveltejswatcher = svelteResponse.data.watchers;
   document.getElementById("vuewatcher").innerHTML = vuewatcher;
-  return data.data.watchers;
-  // .then(function (response) {
-  //   console.log(response.data);
-  //   let vuewatcher = response.data.watchers;
-  //   document.getElementById("vuewatcher").innerHTML = vuewatcher;
+  document.getElementById("angularwatcher").innerHTML = angularwatcher;
+  document.getElementById("emberjswatcher").innerHTML = emberjswatcher;
+  document.getElementById("sveltejswatcher").innerHTML = sveltejswatcher;
+  
+  return {
+    chart: {
+      vue: {
+        watchers: vueResponse.data.watchers,
+        stars: vueResponse.data.stargazers_count,
+        forks: vueResponse.data.forks,
+      },
+      angular: {
+        watchers: angularResponse.data.watchers,
+        stars: angularResponse.data.stargazers_count,
+        forks: angularResponse.data.forks,
+      },
+      emberjs: {
+        watchers: emberjsResponse.data.watchers,
+        stars: emberjsResponse.data.stargazers_count,
+        forks: emberjsResponse.data.forks,
+      },
+      svelts: {
+        watchers: svelteResponse.data.watchers,
+        stars: svelteResponse.data.stargazers_count,
+        forks: svelteResponse.data.forks,
+      }
+    }
+    // .then(function (response) {
+    //   console.log(response.data);
+    //   let vuewatcher = response.data.watchers;
+    //   document.getElementById("vuewatcher").innerHTML = vuewatcher;
       
-  //   console.log("Vue.js Watchers:" + response.data.watchers);
-  //   return vuewatcher;
-  // this.vuewatcher = response.data.watchers;
-  // console.log("Vue.js Watchers2:" + vuewatcher);
+    //   console.log("Vue.js Watchers:" + response.data.watchers);
+    //   return vuewatcher;
+    // this.vuewatcher = response.data.watchers;
+    // console.log("Vue.js Watchers2:" + vuewatcher);
       
       
-}
-async function showData() {
-  vueData()
-  let vueWatcher = await vueData()
-  const data = {
-    labels: [
-      'Vue',
-      'Angular',
-      'Ember'
-    ],
-    datasets: [{
-      label: 'Watchers',
-      data: [vueWatcher, 1000, 5000],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
+  }
+  async function showData() {
+  
+    let info = await vueData()
+    console.log(info);
+    // let vueWatcher = await vueData()
+    // let angularWatcher = await vueData()
+    // let emberjsWatcher = await vueData()
+    const data = {
+      labels: [
+        'Vue',
+        'Angular',
+        'Ember',
+        'Svelte'
       ],
-      hoverOffset: 4
-    }]
-  };
+      datasets: [{
+        label: 'Watchers',
+        data: [info.chart.vue.watchers, info.chart.angular.watchers, info.chart.emberjs.watchers, info.chart.svelte.watchers],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)',
+          'rgb(255, 105, 86)',
+        ],
+        hoverOffset: 4
+      }]
+    };
  
-  const config = {
-    type: 'pie',
-    data: data,
+    const config = {
+      type: 'pie',
+      data: data,
+    };
+    // === include 'setup' then 'config' above ===
+    var myChart = new Chart(
+      document.getElementById('myChart'),
+      config
+    );
   };
-  // === include 'setup' then 'config' above ===
-  var myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
 }
-showData()
-// window.doStuff = function() {
-//   vueData();
-//   console.log("From other function:" + vueData());
-//   // let's make this a wrapper function
-//   // make other functions async
-//   // get the data once it's done - await the axios request
-//   // make the chart
-// }
-// function makeChart() {
-//   const data = {
-//     labels: [
-//       'Vue',
-//       'Angular',
-//       'Ember'
-//     ],
-//     datasets: [{
-//       label: 'Watchers',
-//       data: ["vuewatcher", 1000, 5000],
-//       backgroundColor: [
-//         'rgb(255, 99, 132)',
-//         'rgb(54, 162, 235)',
-//         'rgb(255, 205, 86)'
-//       ],
-//       hoverOffset: 4
-//     }]
-//   };
- 
-//   const config = {
-//     type: 'pie',
-//     data: data,
-//   };
-//   // === include 'setup' then 'config' above ===
-//   var myChart = new Chart(
-//     document.getElementById('myChart'),
-//     config
-//   );
-// }
-  
-  
-// console.log("VueData:" + vueData());
+showData();
